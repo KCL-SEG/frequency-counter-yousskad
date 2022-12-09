2a193d9487064e166d688d22cc5a89454b1606cd
yousskad <115223695+yousskad@users.noreply.github.com>
2022-12-09 12:13:25 +0100

    Task done

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    holder = items
+    for word in holder:
+        counteR = str(word)
+        if counteR in frequencies.keys():
+            frequencies[counteR] += 1
+        else:
+            frequencies[counteR] = 1
     return frequencies
